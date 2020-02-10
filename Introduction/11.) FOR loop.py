328428dca9dbd10c5892508144cfce462a5098eb
slugskickass <a.cadby@sheffield.ac.uk>
2020-02-10 16:40:14 +0000

    Added corrections

--- Introduction/11.) FOR loop.py
+++ Introduction/11.) FOR loop.py
@@ -1,18 +1,18 @@
 for I in range(10):
     print(I)
+print()
 
-print('')
 
-for I in range(20, 30, 2):
-    print(I)
-
+for snuggle in range(20, 30, 2):
+    print(snuggle)
+print()
 
 X = [1, 2, 3, 4, 5, 'Hello', 6, 7, 8, 'World']
 for I in X:
     print(I)
 
 
-for X in range(10):
-    for Y in range(X):
-        print(Y, end = '')
-    print(' ')
+# for X in range(10):
+#     for Y in range(X):
+#         print(Y, end = '')
+#     print(' ')

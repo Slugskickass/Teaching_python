38c0f7410d0e964846b96e2ebc090129754d242a
slugskickass <a.cadby@sheffield.ac.uk>
2020-02-13 23:53:08 +0000

    Nearly done, all done

--- Week 2/1.) Functions.py
+++ Week 2/1.) Functions.py
@@ -1,17 +1,17 @@
 
-
-def myfunction_one(number_of_iterations):
-    for J in range(number_of_iterations):
+def myfunction_one(number_of_iterations):       # Here we define the function using the def command and give it
+    for J in range(number_of_iterations):       # The ability to accept data in this case a number
         for K in range(J):
             print("o", end='')
         print('')
 
-def my_function_two(number_in_one):
+def my_function_two(number_in_one = 100):       # Again defined by the def command, here we define the default value
     flag = True
     for divisor in range(2,number_in_one):
         if number_in_one % divisor == 0:
             flag = False
-    return(flag)
+    return(number_in_one, flag)                 # We also return a value back to the original program.
 
 myfunction_one(10)
 print('Is the number prime, ', my_function_two(97))
+print('Is the number prime, ', my_function_two())
---
+++ Week 2/12.) Basic Images.py
@@ -0,0 +1,21 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+file_name = '../Week 2/Data/640.tif'
+img = Image.open(file_name)
+print('The Image is', img.size, 'Pixels.')
+print('With', img.n_frames, 'frames.')
+
+
+imgArray = np.zeros((img.size[1], img.size[0], img.n_frames), np.uint16)
+for I in range(img.n_frames):
+    img.seek(I)                 # Pointing
+    imgArray[:, :, I] = img     # copying
+img.close()
+
+for I in range(1,10):
+    plt.subplot(3, 3, I)
+    plt.imshow(imgArray[:,:,I-1])
+plt.show()
+
